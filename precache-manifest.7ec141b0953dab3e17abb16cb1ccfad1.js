54cb813e4b99f51f481ab41353b5682aa6c544be
WillYip716 <william.yip.716@gmail.com>
2020-07-26 17:57:43 -0700

    Updates

---
+++ precache-manifest.7ec141b0953dab3e17abb16cb1ccfad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1243459aa5485950d150968cf94c68a",
+    "url": "/blog-admin-viewer/index.html"
+  },
+  {
+    "revision": "4aa422b86d9fda42bf42",
+    "url": "/blog-admin-viewer/static/js/2.bcbdce26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog-admin-viewer/static/js/2.bcbdce26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1ed1a2cb0e63883b01",
+    "url": "/blog-admin-viewer/static/js/main.6e7733bc.chunk.js"
+  },
+  {
+    "revision": "b28cd2a27fa3f81a7a0c",
+    "url": "/blog-admin-viewer/static/js/runtime-main.4d4f382b.js"
+  }
+]);
